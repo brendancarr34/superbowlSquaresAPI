2be9c7ac7437b72c9e015579bb95a66e00c2e674
Brendan Carr <brendan.c34@gmail.com>
2023-11-28 00:41:17 -0800

    SBS-API-003: add create group endpoint

--- controllers/groupController.js
+++ controllers/groupController.js
@@ -3,24 +3,40 @@ const admin = require('firebase-admin');
 
 const router = express.Router();
 
-// Add a document to Firestore with a specified document ID
-router.post('/addDocument/:docId', async (req, res) => {
-  try {
-    const docId = req.params.docId;
-    const data = req.body;
+// Add a group to Firestore with a specified group ID
+router.post('/add/:groupId', async (req, res) => {
+    try {
+        const groupId = req.params.groupId;
+        const data = req.body;
+  
+        if (!groupId) {
+        return res.status(400).json({ error: 'group ID is required' });
+        }
+  
+        const docRef = admin.firestore().collection('group').doc(groupId);
 
-    if (!docId) {
-      return res.status(400).json({ error: 'Document ID is required' });
+        var firestoreMessage = '';
+
+        docRef.create(data)
+            .then(() => {
+                firestoreMessage = 'Document added successfully.';
+                res.status(201).json({ message: firestoreMessage, documentId: groupId });
+            })
+            .catch(error => {
+                if (error.code === 6) {
+                    // Document already exists, handle accordingly
+                    console.log('Document already exists.');
+                    firestoreMessage = 'Document already exists.';
+                    res.status(500).json({ error: firestoreMessage });
+                } else {
+                    firestoreMessage = 'Error adding document:' + error;
+                    res.status(500).json({ error: firestoreMessage });
+                }
+            });
+    } catch (error) {
+        console.error('Error adding document to Firestore:', error);
+        res.status(500).json({ error: 'Internal server error' });
     }
-
-    const docRef = admin.firestore().collection('group-collection').doc(docId);
-    await docRef.set(data);
-
-    res.status(201).json({ message: 'Document added successfully', documentId: docId });
-  } catch (error) {
-    console.error('Error adding document to Firestore:', error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
+  });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -21,74 +21,13 @@ const port = 3001;
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('superbowlsquares-db1 connection established');
+  });
+
 app.use('/api/group', groupController);
 
 app.use('/api/game', gameController);
-
-app.get('/', (req, res) => {
-    res.send('Firebase Admin SDK initialized successfully!');
-  });
-
-  app.get('/api/data', async (req, res) => {
-    try {
-      const snapshot = await admin.firestore().collection('group').get();
-      const data = snapshot.docs.map(doc => doc.data());
-      res.json(data);
-    } catch (error) {
-      console.error('Error retrieving data from Firestore:', error);
-      res.status(500).json({ error: 'Internal server error' });
-    }
-  });
-
-/*
-app.get('/', (req, res) => {
-    res.send('Firebase Admin SDK initialized successfully!');
-  });
-
-app.get('/api/data', async (req, res) => {
-  try {
-    const snapshot = await admin.firestore().collection('collection1').get();
-    const data = snapshot.docs.map(doc => doc.data());
-    res.json(data);
-  } catch (error) {
-    console.error('Error retrieving data from Firestore:', error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
-
-app.post('/api/addDocument', async (req, res) => {
-    try {
-      const data = req.body; // Assuming the request body contains the data for the new document
-    //   res.json(data.name);
-      const result = await admin.firestore().collection('collection1').add(data);
-  
-      res.status(201).json({ message: 'Document added successfully', documentId: result.id });
-    } catch (error) {
-      console.error('Error adding document to Firestore:', error);
-      res.status(500).json({ error: 'Internal server error ' + error });
-    }
-  });
-
-  app.post('/api/addDocument/:docId', async (req, res) => {
-    try {
-      const docId = req.params.docId;
-      const data = req.body;
-  
-      if (!docId) {
-        return res.status(400).json({ error: 'Document ID is required' });
-      }
-  
-      const docRef = admin.firestore().collection('collection1').doc(docId);
-      await docRef.set(data);
-  
-      res.status(201).json({ message: 'Document added successfully', documentId: docId });
-    } catch (error) {
-      console.error('Error adding document to Firestore:', error);
-      res.status(500).json({ error: 'Internal server error' });
-    }
-  });
-
-*/
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
