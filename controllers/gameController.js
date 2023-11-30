f332f0a6664d85354f9704aaf2a363e0732d4740
Brendan Carr <brendan.c34@gmail.com>
2023-11-30 02:11:32 -0800

    SBS-API-006: more claim squares impl

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -34,6 +34,7 @@ router.get('/:groupId', async (req, res) => {
         if (!groupId) {
             return res.status(400).json({ error: 'Group ID is required' });
         }
+        console.log('GET game for group: ' + groupId);
 
         const documentRef = admin.firestore().collection('group').doc(groupId);
 
@@ -55,20 +56,45 @@ router.post('/claimSquares/:groupId', async (req, res) => {
         const groupId = req.params.groupId;
         const data = req.body;
 
-        if (!groupId) {
-            return res.status(400).json({ error: 'Group ID is required' });
-        }
+        if (!groupId) return res.status(400).json({ error: 'Group ID is required' });
 
         const documentRef = admin.firestore().collection('group').doc(groupId);
-
         const doc = await documentRef.get();
 
-        if (!doc.exists) {
-            return res.status(404).json({ error: 'Document not found' });
+        if (!doc.exists) return res.status(404).json({ error: 'Document not found' });
+
+        const existingGroupData = doc.data();
+
+        // console.log('row0: ' + existingGroupData.gameData.row0);
+        // console.log('row0_2: ' + existingGroupData.gameData['row0']);
+
+        // console.log('groupid: ' + groupId);
+        // console.log('data: ' + data.activeButtonData);
+
+        // console.log('activeButtonData[0]: ' + data.activeButtonData[0]);
+
+        
+        for (let i = 0; i < 10; i++) {
+            const rowString = 'row' + i;
+            for (let j = 0; j < 10; j++) {
+                // if (activeButtonData[i][j] === true) {
+                //     gameData[i][j] = true;
+                //     gameNameData[i][j] = playerInitials;
+                // }
+                
+                // console.log('gameData: ' + j + ' ' + existingGroupData.gameData[rowString][j]);
+                // console.log('activeButtonData: ' + data.activeButtonData[i][j])
+                // console.log('\n\n\n')
+                if (data.activeButtonData[i][j] === true && existingGroupData.gameData[rowString][j] === false) {
+                    console.log('found available slot at (' + i + ',' + j + ')!')
+                } else if (data.activeButtonData[i][j] === true && existingGroupData.gameData[rowString][j] === true) {
+                    console.log('a selected square has already been claimed! ');
+                }
+            }
         }
 
-        console.log('data: ' + data);
-        console.log('doc: ' + doc);
+        console.log('initials: ' + data.initials);
+        console.log('name: ' + data.name);
         
     } catch (error) {
         res.status(500).json({ error: 'Error' + error});
