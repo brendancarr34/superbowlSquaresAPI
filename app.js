cea1c731d10e739e5ae4b50ae1fcafdb82f0cb7e
Brendan Carr <bcarr@brendo-pc2.local>
2024-11-06 08:20:49 -1000

    SBS-API-027: add websocket

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const serviceAccount = require('./superbowlsquares-db1-firebase-adminsdk-8sn7g-38bae2da58.json');
 const groupController = require('./controllers/groupController'); 
 const gameController = require('./controllers/gameController'); 
+const { WebSocketServer } = require('ws');
 
 // Initialize Firebase Admin SDK
 admin.initializeApp({
@@ -28,9 +29,35 @@ app.use('/api/group', groupController);
 
 app.use('/api/game', gameController);
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
   });
+
+// Attach WebSocket server to the HTTP server
+const wss = new WebSocketServer({ server });
+
+// Handle WebSocket connection
+wss.on('connection', (ws) => {
+  console.log('New client connected');
+
+  const db = admin.firestore();
+
+const collectionRef = db.collection('group');
+const unsubscribe = collectionRef.onSnapshot((snapshot) => {
+  // Structure data as an array of objects with doc.id: doc.data()
+  const data = snapshot.docs.map((doc) => ({
+      [doc.id]: doc.data()
+  }));
+
+  ws.send(JSON.stringify(data));
+});
+
+  // Handle WebSocket close
+  ws.on('close', () => {
+      console.log('Client disconnected');
+      unsubscribe();  // Stop listening to Firestore updates
+  });
+});
 
 // const host = '10.0.0.65';
 // app.listen(port, host, () => {
